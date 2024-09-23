36d79db0b3b0991ecc8e0a6a6cc713138a3080f4
EmeehM <120816866+EmeehM@users.noreply.github.com>
2024-09-23 20:56:06 -0300

    Creo que ahora si?

--- config.js
+++
@@ -1,8 +0,0 @@
-import * as process from './process';
-require('dotenv').config();
-
-const config = {
-    API_KEY_SUPABASE: process.env.REACT_API_KEY_SUPABASE,
-    API_URL_SUPABASE : process.env.REACT_API_KEY_SUPABASE,
-    CODIGO_ACCESO: process.env.REACT_APP_CODIGO_ACCESO
-};
