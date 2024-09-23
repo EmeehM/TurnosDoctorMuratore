7fe9bbe75e3230c9f46f18b048979fb2356a89db
EmeehM <120816866+EmeehM@users.noreply.github.com>
2024-09-23 20:22:39 -0300

    Bugfix4

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    API_KEY_SUPABASE: process.env.REACT_API_KEY_SUPABASE,
+    API_URL_SUPABASE : process.env.REACT_API_KEY_SUPABASE,
+    CODIGO_ACCESO: process.env.REACT_APP_CODIGO_ACCESO
+};
